ba3198f0b78b5174d7a1d0ddb313471a82ff5b78
ham144 <24434muhammad.yafizham@gmail.com>
2024-10-08 13:07:57 +0700

    please

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,13 @@ app.use("/uploads", express.static(path.join(__dirname + "/uploads")))
 //menampilkan frontend yang sudah di build, kalau edit fe harus rebuild
 app.use(express.static(path.join(__dirname, "/frontend/dist")))//untuk home
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../MERN Movies/frontend/dist", "index.html"))//untuk lain
+    if (process.env.NODE_ENV === "development") {
+        res.sendFile(path.resolve(__dirname, "../MERN Movies/frontend/dist", "index.html"))//untuk lain
+    }
+    else {
+        console.log(path.resolve(__dirname, "../MERN Movies/frontend/dist", "index.html").toString())
+        res.send(path.resolve(__dirname, "../MERN Movies/frontend/dist", "index.html").toString())
+    }
 })
 
 //exported for testing only
